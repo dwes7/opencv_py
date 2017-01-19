be0035642b2a576ac4efef8f69b5b5db494024a7
dwes7 <dwestley.1992@gmail.com>
2017-01-19 16:31:21 -0700

    got gate detector to detect from 8.5 meters. Need to push to mars rover repository

--- Joshi_practice/Chapter 1/geometric_transforms.py
+++ Joshi_practice/Chapter 1/geometric_transforms.py
@@ -1,12 +1,28 @@
 import cv2
-
 import numpy as np
+import argparse
 
-img = cv2.imread('images/gate1.jpg')
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="Path to the source image")
+args = vars(ap.parse_args())
+
+img = cv2.imread(args["image"])
 num_rows, num_cols = img.shape[:2]
+#######################################################################
+#                   Image Translation
+#######################################################################
 
-rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2),
-                                          30, 1)
-img_rotation = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
-cv2.imshow('Rotation', img_rotation)
-cv2.waitKey()
+translation_matrix = np.float32([[1, 0, 70], [0, 1, 110]])
+img_translation = cv2.warpAffine(img, translation_matrix, (num_cols+ 70, num_rows110))
+cv2.imshow('Translation', img_translation)
+cv2.waitKey(0)
+#######################################################################
+#                   Image rotation
+#######################################################################
+
+# rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2),
+#                                           30, 1)
+# img_rotation = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
+# cv2.imshow('Rotation', img_rotation)
+# cv2.waitKey()
--- gate_detection/gate_detector.py
+++ gate_detection/gate_detector.py
@@ -1,27 +1,75 @@
+
 import cv2
-import numpy
-import imutils
+import numpy as np
 
-image = cv2.imread('gate1.jpg')
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-edged_im = cv2.Canny(image, 200, 255)
-# ratio = image.shape[0] / 500.0
-# orig = image.copy()
-# image = imutils.resize(image, height=500)
-#
-# # convert the image to grayscale, blur it, and find edges
-# # in the image
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# gray = cv2.GaussianBlur(gray, (5, 5), 0)
-# edged = cv2.Canny(gray, 75, 200)
+def getthresholdedimg(hsv):
+    # type: (object) -> object
+    threshImg = cv2.inRange(hsv, np.array((cv2.getTrackbarPos('Hue_Low', 'Trackbars'),
+                                           cv2.getTrackbarPos('Saturation_Low', 'Trackbars'),
+                                           cv2.getTrackbarPos('Value_Low', 'Trackbars'))), np.array((cv2.getTrackbarPos(
+        'Hue_High', 'Trackbars'), cv2.getTrackbarPos('Saturation_High', 'Trackbars'), cv2.getTrackbarPos('Value_High',
+                                                                                                         'Trackbars'))))
 
-# show the original image and the edge detected image
-print "STEP 1: Edge Detected"
-cv2.imshow("Image", image)
-cv2.imshow("gray", gray)
-cv2.imshow("blurred", blur)
-cv2.imshow("Edged", edged_im)
-cv2.waitKey(0)
+
+    return threshImg
+
+
+def getTrackValue(value):
+    return value
+
+
+c = cv2.VideoCapture(0)
+width, height = c.get(3), c.get(4)
+print "frame width and height : ", width, height
+
+cv2.namedWindow('Output')
+cv2.namedWindow('Trackbars', cv2.WINDOW_NORMAL)
+cv2.createTrackbar('Hue_Low', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Hue_High', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Value_Low', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Value_High', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Saturation_Low', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Saturation_High', 'Trackbars', 0, 255, getTrackValue)
+cv2.createTrackbar('Caliberate', 'Trackbars', 0, 1, getTrackValue)
+cv2.createTrackbar('Box filter', 'Trackbars', 0, 10000, getTrackValue)
+
+while (1):
+    grabbed, frame = c.read()
+    # this command flips the frame around the y axis
+    frame = cv2.flip(frame, 1)
+
+    blur = cv2.medianBlur(frame, 3)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    thrImg = getthresholdedimg(hsv)
+    erode = cv2.erode(thrImg, None, iterations=2)
+    dilate = cv2.dilate(erode, None, iterations=2)
+
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cx, cy = x + w / 2, y + h / 2
+
+        if (abs(h * w)**2) > cv2.getTrackbarPos('Box filter', 'Trackbars'):
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), [0, 0, 255], 2)
+            print "******tennis ball detected!!***********"
+
+    if cv2.getTrackbarPos('Caliberate', 'Trackbars') == 1:
+        cv2.imshow('Output', thrImg)
+    else:
+        cv2.imshow('Output', frame)
+
+    # cv2.waitKey(50)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
 cv2.destroyAllWindows()
+c.release()
+
+# What would I need to pass to the controller when implementing in ROS for honing?
+# - bool: ballFound
+# - rectangle size
+# - hsv after filtering
+
